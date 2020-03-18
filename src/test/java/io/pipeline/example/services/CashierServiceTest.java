b7c03a5d056196c2a2bc8e2454aa9432907d88a8
Sharath Bhaskara <sbhaskara@connectall.com>
2020-03-18 15:30:23 -0700

    A unit test added

---
+++ src/test/java/io/pipeline/example/services/CashierServiceTest.java
@@ -0,0 +1,14 @@
+package io.pipeline.example.services;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class CashierServiceTest {
+
+	@Test
+	public void testGeBillableOrderByCustomer() throws Exception {
+		Assert.assertTrue(true);
+	}
+
+}
