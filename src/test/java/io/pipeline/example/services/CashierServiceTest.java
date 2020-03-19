016b1093bfa50c7baf5a508b838439025ad20df0
Sharath Bhaskara <sbhaskara@connectall.com>
2020-03-19 08:35:02 -0700

    Implemented the story

--- src/test/java/io/pipeline/example/services/CashierServiceTest.java
+++ src/test/java/io/pipeline/example/services/CashierServiceTest.java
@@ -1,5 +1,7 @@
 package io.pipeline.example.services;
 
+import javax.ws.rs.core.Response;
+
 import org.junit.Test;
 
 import junit.framework.Assert;
@@ -8,6 +10,14 @@ public class CashierServiceTest {
 
 	@Test
 	public void testGeBillableOrderByCustomer() throws Exception {
+		
+		try {
+			System.out.println(System.getenv("CONNECTALL_HOME"));
+			throw new IllegalArgumentException("Wrong argument");
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
 		Assert.assertTrue(true);
 	}
 
